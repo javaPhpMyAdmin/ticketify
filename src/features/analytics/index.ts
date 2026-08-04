61269e3267c066a7d236337f79ee973265f6483a
javaPhpMyAdmin <chelobat16411@gmail.com>
2026-08-04 15:57:57 -0300

    refactor(query): remove dead useCategoryBreakdown hook

--- src/features/analytics/index.ts
+++ src/features/analytics/index.ts
@@ -1,5 +1,4 @@
 export { useMonthlyTotals } from './hooks/useMonthlyTotals';
-export { useCategoryBreakdown } from './hooks/useCategoryBreakdown';
 export { CategoryBreakdownList } from './components/CategoryBreakdownList';
 export type { CategoryBreakdownListProps } from './components/CategoryBreakdownList';
 export { fetchMonthlyTotals, fetchCategoryBreakdown } from './api';
